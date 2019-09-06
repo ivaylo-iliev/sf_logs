0a7bd3a0b32507b36498f947434c46d6ca6d6dca
Ivaylo Iliev <iliev.ivailo@gmail.com>
2019-09-06 21:02:40 +0300

    Changes in handligh no logs found situation

--- sfLogDownloader.py
+++ sfLogDownloader.py
@@ -1,9 +1,23 @@
 import os.path
 import json
 import argparse
-from concurrent.futures import ThreadPoolExecutor
 import traceback
+from concurrent.futures import ThreadPoolExecutor
 from simple_salesforce import Salesforce, exceptions
+
+
+def download_log(instance, log_url, log_id, headers):
+    try:
+        logUrl = '{instance}{log}/Body'.format(instance=instance, log=log_url)
+        req_result = sf.request.get(logUrl, headers=headers)
+        log_file_name = os.path.join(output_dir, log_id + '.log')
+        with open(log_file_name, mode='w') as log_file:
+            log_file.write(req_result.text)
+        log_file.close()
+        print('Done with log ' + log_id)
+    except Exception as e:
+        print(traceback.format_exc())
+
 
 if not os.path.isdir(os.path.join('.', 'profiles')):
     print('No connection profiles defined. Please use the addConfiguration.py script to create one.')
@@ -37,6 +51,7 @@ with open(file_name) as json_file:
         password = value['password']
         security_token = value['token']
         instance_url = value['instance-url']
+
         if value['type'] is 0:
             domain = 'test'
         else:
@@ -61,22 +76,13 @@ else:
 
 log_data = sf.query_all(
     'Select Id, LogUserId, LogLength, LastModifiedDate, Request, Operation, Application, Status, DurationMilliseconds, SystemModstamp, StartTime, Location FROM ApexLog')
+
 items = list(log_data['records'])
 
-
-def download_log(instance, log_url, log_id, headers):
-    try:
-        logUrl = '{instance}{log}/Body'.format(instance=instance, log=log_url)
-        req_result = sf.request.get(logUrl, headers=headers)
-        log_file_name = os.path.join(output_dir, log_id + '.log')
-        with open(log_file_name, mode='w') as log_file:
-            log_file.write(req_result.text)
-        log_file.close()
-        print('Done with log ' + log_id)
-    except Exception as e:
-        print(traceback.format_exc())
-
-
-with ThreadPoolExecutor(max_workers=100) as executor:
-    for item in items:
-        future = executor.submit(download_log, instance_url, item['attributes']['url'], item['Id'], sf.headers)
+if len(items) == 0:
+    print("No logs to download found. Exiting.")
+    exit(2)
+else:
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for item in items:
+            future = executor.submit(download_log, instance_url, item['attributes']['url'], item['Id'], sf.headers)
